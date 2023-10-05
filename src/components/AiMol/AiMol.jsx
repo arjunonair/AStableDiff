a537a096a5a6445f67f1ecc8d754c3956a205e0d
arjunonair <arjunonair123@gmail.com>
2023-10-05 19:08:20 +0530

    first commit

---
+++ src/components/AiMol/AiMol.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+import './AiMol.css'
+import DEFAULT_IMAGE_URL from '../Assets/me.png'
+
+const API_TOKEN = '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'; // Replace with your actual API key
+
+
+const ImageGenerationForm = () => {
+  const [loading, setLoading] = useState(false);
+  let [output, setOutput] = useState(DEFAULT_IMAGE_URL);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+
+    const input = inputValue;
+    const response = await fetch(
+      'https://api-inference.huggingface.co/models/prompthero/openjourney',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${API_TOKEN}`,
+        },
+        body: JSON.stringify({ inputs: input }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error('Failed to generate image');
+    }
+
+    const blob = await response.blob();
+    setOutput(URL.createObjectURL(blob));
+    setLoading(false);
+  };
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  return (
+    <div className="ai-image-div">
+      <div className="ai-image-header">Stable<span> Diffusion</span></div>
+      <div className="ai-image">
+          {loading && <div className="loading">Loading...</div>}
+          {!loading && (
+            <img src={output} alt="" />
+          )}
+      </div>
+      <div className="ai-image-loading">
+        <form className="ai-image-search" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            autoComplete='o'
+            name="input"
+            className="ai-image-search-input"
+            placeholder="Describe what you want to see..."
+            value={inputValue}
+            onChange={handleInputChange}
+          />
+          <button type="submit" className="ai-image-search-btn">
+            Generate
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ImageGenerationForm;
